59243dd19908c77b8280d00ab75ae71665d1ae06
Simatwa <smartwacaleb@gmail.com>
2023-03-31 18:58:15 +0300

    Update to version - `1.8.3`

--- tdwnsv3/__init__.py
+++ tdwnsv3/__init__.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-__info__ = "Simple local-files server with security as main priority!"
+__info__ = "Simple local-files server with security on top!"
 __version__ = "1.8.3"
 __author__ = "Smartwa Caleb"
 __email__="smartwacaleb@gmail.com"
